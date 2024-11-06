60994b668331c114dfefdb204314e31cad4cb976
ANU7MADHAV <anumadhavan888@gmail.com>
2024-11-07 00:05:37 +0530

    Feat : user create api& findAll

--- apps/backend/metaverse-api/src/user/user.controller.ts
+++ apps/backend/metaverse-api/src/user/user.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { User } from './interfaces/user.interface';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Get()
-  findAll(): string {
-    return 'users []';
+  @HttpCode(200)
+  async findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+  @Post()
+  @HttpCode(201)
+  async create(data: User): Promise<User | string> {
+    return this.userService.create(data);
   }
 }
