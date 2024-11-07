94a6d517af23dac869234abe625a7b0ac08f6580
ANU7MADHAV <anumadhavan888@gmail.com>
2024-11-07 13:07:00 +0530

    Feat : added auth-guard

--- apps/backend/metaverse-api/src/auth/auth.module.ts
+++ apps/backend/metaverse-api/src/auth/auth.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController, AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
+import { AuthController } from './auth.controller';
+import { AuthService, AuthService } from './auth.service';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- apps/backend/metaverse-api/src/auth/auth.service.ts
+++ apps/backend/metaverse-api/src/auth/auth.service.ts
@@ -1,15 +1,23 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { User } from 'src/user/interfaces/user.interface';
+import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async signIn(username: string, pass: string): Promise<any> {
     const user = await this.userService.findOne(username);
     if (user?.password !== pass) {
       throw new UnauthorizedException('Invalid credentials');
     }
+    const payload = { sub: user.id, username: user.email };
+
+    return {
+      access_token: this.jwtService.signAsync(payload),
+    };
   }
 }
